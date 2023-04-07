b329729193b1222cfcea17c789fe5d23cb4b5634
MertArar <85193329+MertArar@users.noreply.github.com>
2023-04-07 06:26:23 +0300

    learned until part 5

---
+++ pages/posts/picture.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Picture = () => {
+  <Image
+    src="../../public/images/mertararpp.jpeg"
+    height={144}
+    width={144}
+    alt="mertarar"
+  />;
+};
+
+export default Picture;
